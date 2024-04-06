18c0ccd102a1e7f111c1482f203fa11c4e6b2eff
Tamas Nemes <nemestomi2@gmail.com>
2024-04-07 01:37:56 +0200

    Create store for materials

---
+++ src/components/MaterialSelector.tsx
@@ -0,0 +1,11 @@
+import { Tabs, Tab, Card, CardBody } from "@nextui-org/react";
+
+export default function MaterialSelector() {
+    return (
+        <div className="flex w-full flex-col">
+            <Tabs aria-label="Semester">
+                
+            </Tabs>
+        </div>
+    );
+}
---
+++ src/data/materials.ts
@@ -0,0 +1,94 @@
+interface SemesterMaterials {
+    id: string,
+    materials: LectureMaterial[],
+}
+
+interface LectureMaterial {
+    title: string,
+    unitNumber: number,
+    links: Link[],
+    unlockTime: Date,
+}
+
+interface Link {
+    title: string,
+    uri: string,
+}
+
+export const materials: SemesterMaterials[] = [
+    {
+        id: "SS 2024",
+        materials: [
+            {
+                title: "Ordinary Least Squares (OLS) Demo",
+                unitNumber: 1,
+                links: [
+                    {
+                        title: "Desmos",
+                        uri: "https://www.desmos.com/calculator/r9fm4syebi?lang=de"
+                    }
+                ],
+                unlockTime: new Date("2024-04-12T16:00:00"),
+            },
+            {
+                title: "Die Kostenfunktion (Animiertes GIF)",
+                unitNumber: 1,
+                links: [
+                    {
+                        title: "Gifyu",
+                        uri: "https://gifyu.com/image/SnwYe"
+                    }
+                ],
+                unlockTime: new Date("2024-04-12T16:00:00"),
+            },
+            {
+                title: "Mitmachübung: Interaktives Modell eines Perzeptrons",
+                unitNumber: 1,
+                links: [
+                    {
+                        title: "Übung",
+                        uri: "https://totemi1324.github.io/perzeptron-demo/"
+                    }
+                ],
+                unlockTime: new Date("2024-04-12T17:00:00"),
+            },
+            {
+                title: "Sigmoid vs. ReLU Demo",
+                unitNumber: 2,
+                links: [
+                    {
+                        title: "Desmos",
+                        uri: "https://www.desmos.com/calculator/lz8h2iejdx"
+                    }
+                ],
+                unlockTime: new Date("2024-04-13T09:00:00"),
+            },
+            {
+                title: "Gradient descent (Animiertes GIF)",
+                unitNumber: 2,
+                links: [
+                    {
+                        title: "Gifyu",
+                        uri: "https://gifyu.com/image/Snh9O"
+                    }
+                ],
+                unlockTime: new Date("2024-04-13T11:00:00"),
+            },
+            {
+                title: "Mitmachübung: Trainieren eines neuronalen Netzwerks",
+                unitNumber: 2,
+                links: [
+                    {
+                        title: "Colab",
+                        uri: "https://colab.research.google.com/drive/1HVMc-fIU4gWD0SiswDBzVSRafJzcFyaX?usp=sharing"
+                    },
+                    {
+                        title: "GitHub",
+                        uri: "https://github.com/Totemi1324/neural-networks-demo"
+                    },
+                ],
+                unlockTime: new Date("2024-04-13T12:00:00"),
+            },
+        ]
+    }
+]
