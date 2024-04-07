b6ec226fde03c5c6e2c1fb3ae5276f1e4d4ce399
Tamas Nemes <nemestomi2@gmail.com>
2024-04-08 01:50:46 +0200

    Add stub item to accordeons

--- src/components/LectureMaterialList.tsx
+++ src/components/LectureMaterialList.tsx
@@ -8,12 +8,14 @@ type LectureMaterialListProps = {
 
 export default function LectureMaterialList({ materials }: LectureMaterialListProps) {
     const currentTime: Date = new Date();
+    let shownItems: number = 0;
 
     return (
         <div className="flex flex-col items-center gap-6">
             {
                 materials.map((material, index) => {
-                    if (/*currentTime >= material.unlockTime*/ true) {
+                    if (currentTime >= material.unlockTime) {
+                        shownItems++;
                         return (
                             <LectureMaterialCard
                                 key={index}
@@ -27,6 +29,9 @@ export default function LectureMaterialList({ materials }: LectureMaterialListPr
                         return null;
                     }
                 })
+            }
+            {
+                shownItems == 0 ? <p className="text-gray-500 text-center">Noch nichts zum Anzeigen...</p> : null
             }
         </div>
     );
