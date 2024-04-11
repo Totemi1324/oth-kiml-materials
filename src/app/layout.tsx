14facc142b63efd69ca417ddf3334753c43330c9
Tamas Nemes <nemestomi2@gmail.com>
2024-04-12 01:10:05 +0200

    Add web analytics module

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 
 import { Providers } from "./providers";
 import { primary_font } from "@/ui/fonts";
@@ -23,6 +24,7 @@ export default function RootLayout({
           {children}
         </Providers>
         <SpeedInsights/>
+        <Analytics />
       </body>
     </html>
   );
